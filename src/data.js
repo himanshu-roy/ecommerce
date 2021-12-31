d768f50bfa523a17f45f8139c3cd568fbaaa6ea2
Himanshu Roy <himanshu.roy@rentomojo.com>
2021-12-31 18:27:30 +0530

    (frontend): added sliders on homepage

---
+++ src/data.js
@@ -0,0 +1,21 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/wsgTKtm/undraw-jewelry-iima.png",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS."
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/pfzCnKj/undraw-farm-girl-dnpe.png",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS."
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/XSGBxL0/undraw-girl-just-wanna-have-fun-9d5u.png",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF ON NEW ARRIVALS."
+    },
+    
+]
